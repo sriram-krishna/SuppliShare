9443d2d319bbd1182566cb5168889ecf29e8efce
DevilTriggerDante <145049142+DevilTriggerDante@users.noreply.github.com>
2023-10-19 20:08:03 -0500

    Made the edit email page

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser=require('body-parser');
 const app = express();
 const PORT = 5000; // You can choose any port you like
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello, SuppliShare!');
 });
 
+app.post('/', (req,res)=>{
+    console.log(req.body)
+    res.json({message:`Your new Email is ${req.body.email}`});
+});
+
+app.all("*",(req,res)=>{
+    console.log('wildcard')
+    res.status(404).send("Not Found")
+})
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`The Server is running on http://localhost:${PORT}`);
 });
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import {useState} from 'react';
 function App() {
+  const [email,setEmail] = useState("")
+
+  function handleEmailChange(event){
+  setEmail(event.target.value)
+  }
+ 
+  function submit(event){ 
+    event.preventDefault()
+    fetch("http://localhost:5000",{
+      method:"POST", mode:"cors", body:JSON.stringify({email:email}),headers:{
+        Accept:"application/json","Content-Type": "application/json"
+      }
+    })
+    .then(response=>response.json())
+    .then(responseJSON=>console.log(responseJSON.message))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1><b>Edit Email</b></h1>
+      <form>
+        {/* <label>Current Email:</label><br /> */}
+        <label>New Email: </label><br />
+        <input type="text" id="newEmail" value={email} onChange={handleEmailChange} /><br /><br />
+        <input type="submit" value="Submit" onClick={submit} />
+    </form>
       </header>
+     
     </div>
   );
 }
