61a3c51e63255cf40060b9c9ac4aa85cb145e984
DevilTriggerDante <145049142+DevilTriggerDante@users.noreply.github.com>
2023-10-26 12:54:29 -0500

    Made a note for later

--- backend/src/routes/accountRoutes.js
+++ backend/src/routes/accountRoutes.js
@@ -9,9 +9,10 @@ router.post('/change-password', async (req, res) => {
         const { username, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         console.log(req.body);
-        console.log(hashedPassword);
+        console.log(hashedPassword); 
         userModel.changeUserPassword(username, password);
         res.status(200).json({message:`Your new password is ${password}`});
+        //need to upload new password to database here
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
