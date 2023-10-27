868a75b7c6a5dd9d2c38f7e8352ccfed5176ea47
DevilTriggerDante <145049142+DevilTriggerDante@users.noreply.github.com>
2023-10-27 18:53:39 -0500

    Made the <a> tag

--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -10,8 +10,8 @@ function LoginView() {
             </header>
             <main className="login-view-main"> 
                 {/*<LoginComponent /> */}
-                <button class="CreateNewAccount">Click here to sign up!</button> 
-                <button class="LoginButton" style={ {background:'#d782ff'}}>Already have an acoount? Click here to sign in!</button>
+                <button class="CreateNewAccount" >Click here to sign up!</button> 
+                <button class="LoginButton" style={ {background:'#d782ff'}}><a href="/components/Account-Settings/Home"> </a>Already have an acoount? Click here to sign in!</button>
             </main>These two lines should be put in the landing page
         </div>
     );
