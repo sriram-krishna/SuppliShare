15d5cfcbb6cd85a4a0b1c0175e0237e74967e681
DevilTriggerDante <145049142+DevilTriggerDante@users.noreply.github.com>
2023-10-28 15:05:25 -0500

    Added a centered section to the css file

--- frontend/src/views/Login/Landing-page.js
+++ frontend/src/views/Login/Landing-page.js
@@ -9,13 +9,16 @@ function LandingPage() {
                 <h1 className="landingPage-view-app-title">SuppliShare</h1>
             </header>
             <main className="LandingPage-main"> 
+                <div className='centered'>
                 {/* <LoginComponent /> */}
                 {/* <button class="CreateNewAccount" >Click here to sign up!</button> 
                 <button class="LoginButton" style={ {background:'#d782ff'}}><a href="/components/Account-Settings/Home"> </a>Already have an acoount? Click here to sign in!</button> */}
                     {/* <button class="CreateNewAccount" type="button">Click here to sign up!</button>
                     <button class="LoginButton" type="button" >Already have an acoount? Click here to sign in!</button> */}
                 <button style={{ backgroundColor: '#82c3ff', width: '120px', height: '40px', fontFamily: 'Trebuchet MS', borderRadius: '15px', marginBottom: '10px', color: 'white' }}>Click here to sign up!</button>
-                <button style={{ backgroundColor: '#d782ff', width: '120px', height: '40px', fontFamily: 'Trebuchet MS', borderRadius: '15px', color: 'white' }}>Already have an account? Click here to sign in!</button>
+                <br></br>
+                <button style={{ backgroundColor: '#d782ff', width: '120px', height: '40px', fontFamily: 'Trebuchet MS', borderRadius: '15px', color: 'white' }}>Click here to log-in</button>
+                </div>
             </main>These two lines should be put in the landing page
         </div>
     );
