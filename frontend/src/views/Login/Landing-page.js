792219c66fdeb7aee1934d957297c0f633cbf6fc
DevilTriggerDante <145049142+DevilTriggerDante@users.noreply.github.com>
2023-11-07 11:59:43 -0600

    Merged landing and registration page

--- frontend/src/views/Login/Landing-page.js
+++ frontend/src/views/Login/Landing-page.js
@@ -9,17 +9,24 @@ function LandingPage() {
             <header className="landingPage-header">
                 <h1 className="landingPage-view-app-title">SuppliShare</h1>
             </header>
-            <main className="LandingPage-main"> 
-                
+            <main className="LandingPage-main">
+
                 <div className='centered'>
-                {/* <LoginComponent /> */}
-                {/* <button class="CreateNewAccount" >Click here to sign up!</button> 
+                    {/* <LoginComponent /> */}
+                    {/* <button class="CreateNewAccount" >Click here to sign up!</button> 
                 <button class="LoginButton" style={ {background:'#d782ff'}}><a href="/components/Account-Settings/Home"> </a>Already have an acoount? Click here to sign in!</button> */}
-                
-                <button style={{ backgroundColor: '#82c3ff', width: '120px', height: '40px', fontFamily: 'Trebuchet MS', borderRadius: '15px', marginBottom: '10px', color: 'white' }}>Click here to sign up!</button>
-                <Link to="/Login" className='login-Link'>
-                <button style={{ backgroundColor: '#d782ff', width: '120px', height: '40px', fontFamily: 'Trebuchet MS', borderRadius: '15px', color: 'white'}} on>Click here to log-in</button>
-                </Link>
+
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+                        <h1 style={{ fontFamily: 'Fantasy', color: 'black', fontSize: '32px' }}>Your Home For Help!</h1>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '20px' }}>
+                            <button style={{ backgroundColor: '#82c3ff', width: '120px', height: '40px', fontFamily: 'Trebuchet MS', borderRadius: '15px', marginBottom: '10px', color: 'white' }}>Sign Up as a Donor</button>
+                            <button style={{ backgroundColor: '#d782ff', width: '120px', height: '40px', fontFamily: 'Trebuchet MS', borderRadius: '15px', color: 'white' }}>Sign Up as a Borrower</button>
+                            <Link to="/Login" className='login-Link'>
+                                <button style={{ backgroundColor: '#32CD32', width: '120px', height: '40px', fontFamily: 'Trebuchet MS', borderRadius: '15px', color: 'white' }} on>Click here to log-in</button>
+                            </Link>
+                        </div>
+
+                    </div>
                 </div>
             </main>
         </div>
