8d1267eda8fca02ed6637db8d29b24746eb832a7
Sriram Krishna Kumar <sriramkrishnakumar@my.unt.edu>
2023-11-10 16:59:03 -0600

    changed main to body to test bug

--- frontend/src/views/Login/DonorSignup.jsx
+++ frontend/src/views/Login/DonorSignup.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from "react";
 //import LoginComponent from '../../components/Auth/Login';
-import './Login.css';
-
+import "./Login.css";
 
 function DonorSignUpView() {
-    
-    return (
-        <div className="login-view-wrapper">
-            <header className="login-view-header">
-              <center style={{ color: '#ff9b82', fontSize: '2.5rem', fontFamily: 'fantasy' }}>DONOR</center>
-            </header>
-            <main className="login-view-main">
-                <div className='centered'>
-                    <div id="api"></div>
-                </div>
-            </main>
-        </div>
-    );
+  return (
+    <div className="login-view-wrapper">
+      <header className="login-view-header">
+        <center
+          style={{
+            color: "#ff9b82",
+            fontSize: "2.5rem",
+            fontFamily: "fantasy",
+          }}
+        >
+          DONOR
+        </center>
+      </header>
+      <body>
+        <div id="api"></div>
+      </body>
+    </div>
+  );
 }
 // function redirectToLogin()
 // {
