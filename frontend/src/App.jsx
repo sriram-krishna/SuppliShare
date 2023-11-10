735156e47afd0cca560c1502366d52a13011427f
DevilTriggerDante <145049142+DevilTriggerDante@users.noreply.github.com>
2023-11-10 17:59:33 -0600

    Added links to the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { MsalProvider } from "@azure/msal-react";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { msalConfig } from './authConfig';
+import { Link } from 'react-router-dom';
+
 import LoginView from './views/Login/Login';
 import Header from './components/shared/Header/Header';
 
-import AccountSettings from './components/Account-Settings/AccountSettings';
-import EditEmail from './components/Account-Settings/EditEmail';
-import Home from './components/Account-Settings/Home';
-import EditPassword from './components/Account-Settings/EditPassword';
-import EditZipcode from './components/Account-Settings/EditZipcode';
-import EditSchool from './components/Account-Settings/EditSchool';
+// import AccountSettings from './components/Account-Settings/AccountSettings';
+// import EditEmail from './components/Account-Settings/EditEmail';
+// import Home from './components/Account-Settings/Home';
+// import EditPassword from './components/Account-Settings/EditPassword';
+// import EditZipcode from './components/Account-Settings/EditZipcode';
+// import EditSchool from './components/Account-Settings/EditSchool';
 import LandingPage from './views/Login/Landing-page';
 import ForgotPasswordEmailSubmission from './views/Login/ForgotPassword';
 import DonorSignUpView from './views/Login/DonorSignup';
-import TeacherSignUpView from './views/Login/DonorSignup';
+import TeacherSignUpView from './views/Login/TeacherSignup';
 import WatermarkComponent from './views/Login/watermark';
 
 const msalInstance = new PublicClientApplication(msalConfig);
@@ -31,24 +33,29 @@ function App() {
     <MsalProvider instance={msalInstance}>
       <Router>
         <Header showSearch={true} user={{ firstName: 'John' }} handleSetLoggedIn={handleSetHeaderState} loggedIn={headerState} />
-        
-		<WatermarkComponent/>
+
+        <WatermarkComponent />
         <Routes>
           <Route
             path="/"
             element={<LandingPage handleSetHeaderState={handleSetHeaderState} />}
           />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<LandingPage />} />
+          <Link to='./views/Login/Landing-page'></Link>
           <Route path="/DonorSignUp" element={<DonorSignUpView />} />
-		  <Route path="/TeacherSignUp" element={<TeacherSignUpView />} />
+          <Link to="./views/Login/DonorSignup"></Link>
+          <Route path="/TeacherSignUp" element={<TeacherSignUpView />} />
+          <Link to='./views/Login/TeacherSignupSignup'></Link>
           <Route path="/Login" element={<LoginView />} />
+          <Link to='./views/Login/Landing-page'></Link>
           <Route path="/ForgotPassword" element={<ForgotPasswordEmailSubmission />} />
-          <Route path="/EditSettings" element={<Home />} />
+          <Link to='./views/Login/ForgotPassword'></Link>
+          {/* <Route path="/EditSettings" element={<Home />} />
           <Route path="/AccountSettings" element={<AccountSettings />} />
           <Route path="/EditEmail" element={<EditEmail />} />
           <Route path="/EditPassword" element={<EditPassword />} />
           <Route path="/EditZipcode" element={<EditZipcode />} />
-          <Route path="/EditSchool" element={<EditSchool />} />
+          <Route path="/EditSchool" element={<EditSchool />} /> */}
           {/* other routes */}
         </Routes>
       </Router>
