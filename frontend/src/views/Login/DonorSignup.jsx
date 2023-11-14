f76417f46f11bcd685a2698bd85fa6cc2649bf38
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2023-11-14 12:47:35 -0600

    d
    test api

--- frontend/src/views/Login/DonorSignup.jsx
+++ frontend/src/views/Login/DonorSignup.jsx
@@ -3,27 +3,14 @@ import "./Login.css";
 
 function DonorSignUpView() {
   return (
-    <div className="login-view-wrapper">
             <header>
               <center style={{ color: '#ff9b82', fontSize: '2.5rem', fontFamily: 'fantasy' }}>Donor</center>
             </header>
-            <main className="login-view-main">
-                <div className='centered'>
-                    {/*<LoginComponent /> */}
-                    {/* <button class="CreateNewAccount" >Click here to sign up!</button> 
-                        <button class="LoginButton" style={ {background:'#d782ff'}}><a href="/components/Account-Settings/Home"> </a>Already have an acoount? Click here to sign in!</button> */}
-                    <form autoComplete='off'>
-                        <label>Email: </label><br />
-                        <input type="text" id="Email" />
-                        <label>Password: </label><br />
-                        <input type="password" id="password" />
-                        <label>Zipcode: </label>
-                        <input type="number" id="Zipcode"  />
-                    </form>
-                    <input type="submit" value="Submit" />
-                </div>
-            </main>
-        </div>
+			<div id="api">
+                
+		    </div>
+            
+        
   );
 }
 export default DonorSignUpView;
