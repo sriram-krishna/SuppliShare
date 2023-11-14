7b5a5216094648d38422a3d63130bca4c2987bf0
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2023-11-14 12:52:40 -0600

    divid
    test

--- frontend/src/views/Login/DonorSignup.jsx
+++ frontend/src/views/Login/DonorSignup.jsx
@@ -3,14 +3,16 @@ import "./Login.css";
 
 function DonorSignUpView() {
   return (
+    <div className="login-view-wrapper">
             <header>
               <center style={{ color: '#ff9b82', fontSize: '2.5rem', fontFamily: 'fantasy' }}>Donor</center>
             </header>
+            <main className="login-view-main">
 			<div id="api">
                 
 		    </div>
-            
-        
+            </main>
+        </div>
   );
 }
 export default DonorSignUpView;
