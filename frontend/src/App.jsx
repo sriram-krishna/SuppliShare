eec1d984416b0fc4526e240ef1729065bdfc9a58
SinisterNight <114369787+SinisterNight@users.noreply.github.com>
2023-11-15 10:35:00 -0600

    changes to fix visual bug for header
    configured hooks to work, installed plugin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,80 +1,80 @@
-import React, { useState, useEffect} from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import LoginView from './views/Login/Login';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/shared/Header/Header';
-
-import AccountSettings from './components/Account-Settings/AccountSettings';
-import EditEmail from './components/Account-Settings/EditEmail';
-import Home from './components/Account-Settings/Home';
-import EditPassword from './components/Account-Settings/EditPassword';
-import EditZipcode from './components/Account-Settings/EditZipcode';
-import EditSchool from './components/Account-Settings/EditSchool';
 import LandingPage from './views/Login/Landing-page';
-import ForgotPasswordEmailSubmission from './views/Login/ForgotPassword';
 import DonorSignUpView from './views/Login/DonorSignup';
 import TeacherSignUpView from './views/Login/DonorSignup';
 import WatermarkComponent from './views/Login/watermark';
-import ItemUpload from './components/shared/NavBar/itemUpload';
 import Navbar from './components/shared/NavBar/navbar';
-
+import Home from './components/Account-Settings/Home';
+import ForgotPasswordEmailSubmission from './views/Login/ForgotPassword';
 
 function App() {
-  const [headerState, setHeaderState] = useState("loggedout");
-  const [navBarState, setnavBarState] = useState("none");
+  const [headerState, setHeaderState] = useState('loggedout');
+  const [navBarState, setNavBarState] = useState('none');
 
   const handleSetHeaderState = (newState) => {
     setHeaderState(newState);
   };
-  
 
-  // Check if the user is on the homepage
-  const isHomePage = window.location.pathname === '/home';
+  const updateHeaderState = () => {
+    const isHomePage = window.location.pathname === '/home';
+    const isLanding = window.location.pathname === '/';
+    const isDonor = window.location.pathname === '/DonorSignUp';
+
+    if (isHomePage) {
+      setHeaderState('loggedin');
+      setNavBarState('Donor');
+    } else if (isLanding) {
+      setHeaderState('loggedout');
+      setNavBarState('none');
+    } else if (isDonor) {
+      console.log('isDonor is true');
+      setHeaderState('none');
+    } else {
+      setHeaderState('loggedin');
+      setNavBarState('none');
+    }
+  };
 
   // Update header state when the component mounts
   useEffect(() => {
-    if (isHomePage) {
-      setHeaderState("loggedin");
-    } else {
-      setHeaderState("loggedout");
-    }
-  }, [isHomePage]);
+    updateHeaderState();
+  }, []);
+
+  // Listen for changes in the route and update header state
   useEffect(() => {
-    if (isHomePage) {
-      setnavBarState("Donor");
-    } else {
-      setnavBarState("none");
-    }
-  }, [isHomePage]);
+    updateHeaderState();
+  }, [window.location.pathname]);
+
+  // Listen for the popstate event to handle back button clicks
+  useEffect(() => {
+    const handlePopstate = () => {
+      updateHeaderState();
+    };
+
+    window.addEventListener('popstate', handlePopstate);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopstate);
+    };
+  }, []);
 
   return (
-      <Router>
-        <Header showSearch={true} user={{ firstName: 'John' }} handleSetLoggedIn={handleSetHeaderState} loggedIn={headerState} />
-        
-		<WatermarkComponent/>
-		<Navbar navBarState={navBarState} />
-        <Routes>
-		
-          <Route
-            path="/"
-            element={<LandingPage handleSetHeaderState={handleSetHeaderState} />}
-          />
-          <Route path="/home" element={<Home />} />
-          <Route path="/DonorSignUp" element={<DonorSignUpView />} />
-		  <Route path="/TeacherSignUp" element={<TeacherSignUpView />} />
-          <Route path="/Login" element={<LoginView />} />
-          <Route path="/ForgotPassword" element={<ForgotPasswordEmailSubmission />} />
-          <Route path="/EditSettings" element={<Home />} />
-          <Route path="/AccountSettings" element={<AccountSettings />} />
-          <Route path="/EditEmail" element={<EditEmail />} />
-          <Route path="/EditPassword" element={<EditPassword />} />
-          <Route path="/EditZipcode" element={<EditZipcode />} />
-          <Route path="/EditSchool" element={<EditSchool />} />
-		  <Route path="/imageUploader" element={<imageUploader />} />
-		  <Route path="/itemUpload" element={<ItemUpload />} />
+    <Router>
+      <Header showSearch={true} user={{ firstName: 'John' }} handleSetLoggedIn={handleSetHeaderState} loggedIn={headerState} />
 
-          {/* other routes */}
-        </Routes>
-      </Router>
+      <WatermarkComponent />
+      <Navbar navBarState={navBarState} />
+      <Routes>
+        <Route path="/" element={<LandingPage handleSetHeaderState={handleSetHeaderState} />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/DonorSignUp" element={<DonorSignUpView />} />
+        <Route path="/TeacherSignUp" element={<TeacherSignUpView />} />
+        <Route path="/ForgotPassword" element={<ForgotPasswordEmailSubmission />} />
+        {/* other routes */}
+      </Routes>
+    </Router>
   );
 }
 
