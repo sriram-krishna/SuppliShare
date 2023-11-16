fbec7059f0999a78d9a9f10a5d1f1b40bc4bc520
Sriram Krishna Kumar <sriramkrishnakumar@my.unt.edu>
2023-11-16 03:28:28 -0600

    Testing just the login with B2C

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// import { Link } from 'react-router-dom';
-
 import LoginView from './views/Login/Login';
-import Header from './components/shared/Header/Header';
 
-// import AccountSettings from './components/Account-Settings/AccountSettings';
-// import EditEmail from './components/Account-Settings/EditEmail';
-// import Home from './components/Account-Settings/Home';
-// import EditPassword from './components/Account-Settings/EditPassword';
-// import EditZipcode from './components/Account-Settings/EditZipcode';
-// import EditSchool from './components/Account-Settings/EditSchool';
-import LandingPage from './views/Login/Landing-page';
-import ForgotPasswordEmailSubmission from './views/Login/ForgotPassword';
-import DonorSignUpView from './views/Login/DonorSignup';
-import TeacherSignUpView from './views/Login/TeacherSignup';
-import WatermarkComponent from './views/Login/watermark';
 
 function App() {
-  const [headerState, setHeaderState] = useState("loggedout");
-
-  const handleSetHeaderState = (newState) => {
-    setHeaderState(newState);
-  };
-
   return (
       <Router>
-        <Header showSearch={true} user={{ firstName: 'John' }} handleSetLoggedIn={handleSetHeaderState} loggedIn={headerState} />
-
-        <WatermarkComponent />
         <Routes>
-          <Route path="/DonorSignUp" element={<DonorSignUpView />} />
-          <Route path="/TeacherSignUp" element={<TeacherSignUpView />} />
-          <Route
-            path="/"
-            element={<LandingPage handleSetHeaderState={handleSetHeaderState} />}
-          />
-          {/* <Route path="/home" element={<LandingPage />} /> */}
-          {/* <Link to='./views/Login/Landing-page'></Link> */}
-          {/* <Link to="./views/Login/DonorSignup"></Link> */}
-          {/* <Link to='./views/Login/TeacherSignupSignup'></Link> */}
           <Route path="/Login" element={<LoginView />} />
-          {/* <Link to='./views/Login/Landing-page'></Link> */}
-          <Route path="/ForgotPassword" element={<ForgotPasswordEmailSubmission />} />
-          {/* <Link to='./views/Login/ForgotPassword'></Link> */}
-          {/* <Route path="/EditSettings" element={<Home />} />
-          <Route path="/AccountSettings" element={<AccountSettings />} />
-          <Route path="/EditEmail" element={<EditEmail />} />
-          <Route path="/EditPassword" element={<EditPassword />} />
-          <Route path="/EditZipcode" element={<EditZipcode />} />
-          <Route path="/EditSchool" element={<EditSchool />} /> */}
-          {/* other routes */}
         </Routes>
       </Router>
   );
---
+++ frontend/src/views/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+
+function LoginView() {
+    
+    return (
+        <div id="api" role="main"></div>
+    );
+}
+
+export default LoginView;
