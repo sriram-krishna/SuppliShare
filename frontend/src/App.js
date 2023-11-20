8f52425a198365efbdd5a1ada03d87c865452dea
SriramKrish <sriramkrish0110@gmail.com>
2023-11-19 23:56:54 -0600

    Testing the Login Custom UI page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import Home from './views/Home';
-import Login from './views/Login';
+import Home from "./views/Home";
+import Login from "./views/Login";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
+    <div className="App">
+      <Routes>
         <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          </Routes>
-      </div>
-    </Router>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import './styles/index.css';
-import App from './App';
+import "./styles/index.css";
+import App from "./App";
 
-const root = createRoot(document.getElementById('root'));
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
