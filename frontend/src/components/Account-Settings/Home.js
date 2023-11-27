c3b5b9435dc4634d9d981dca15c97c5d1fc23ad8
mghazi002 <muhammadghazi@my.unt.edu>
2023-11-26 21:39:34 -0600

    Revert "deleted fe"
    This reverts commit 1d49bace379d4c7bbce5996bb52fc4cd76f02938.

---
+++ frontend/src/components/Account-Settings/Home.js
@@ -0,0 +1,18 @@
+//Need to rename this file to SettingsHome
+
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './AccountSettings.css';
+
+const Home = () => {
+  return (
+    <div>
+      <header className="Home-header">
+        <Link to="/AccountSettings"> Go to Account Settings</Link>
+      </header>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/views/Login/DonorSignup.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+//import LoginComponent from '../../components/Auth/Login';
+import './Login.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function DonorSignUpView() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [zipcode, setZipcode] = useState("")
+    function submit(event) {
+        event.preventDefault() //Prevents default behavior of the form when trying to submit
+    }
+    return (
+        <div className="login-view-wrapper">
+            <header className="login-view-header">
+              <center style={{ color: '#ff9b82', fontSize: '2.5rem', fontFamily: 'fantasy' }}>DONOR</center>
+            </header>
+            <main className="login-view-main">
+                <div className='centered'>
+                    {/*<LoginComponent /> */}
+                    {/* <button class="CreateNewAccount" >Click here to sign up!</button> 
+                        <button class="LoginButton" style={ {background:'#d782ff'}}><a href="/components/Account-Settings/Home"> </a>Already have an acoount? Click here to sign in!</button> */}
+                    <form autoComplete='off'/*className='login-page'*/>
+                        <label>Email: </label><br />
+                        <input type="text" id="Email" onChange={(e) => setEmail(e.target.value)} />
+                        <label>Password: </label><br />
+                        <input type="password" id="password" onChange={(e) => setEmail(e.target.value)}/>
+                        <label>Zipcode: </label>
+                        <input type="number" id="Zipcode" onChange={(e) => setZipcode(e.target.value)} />
+                    </form>
+                    <input type="submit" value="Submit" />
+                </div>
+            </main>
+        </div>
+    );
+}
+// function redirectToLogin()
+// {
+//     var url = "/Login";
+//     window.location.href= url;
+// }
+export default DonorSignUpView;
