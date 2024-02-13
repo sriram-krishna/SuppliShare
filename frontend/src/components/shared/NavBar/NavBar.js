c4b6677ac79d41ac0f87a77f027be92653172244
sriram-krishna <117504779+sriram-krishna@users.noreply.github.com>
2024-02-13 14:13:47 -0600

    Removed console.log

--- frontend/src/components/shared/NavBar/NavBar.js
+++ frontend/src/components/shared/NavBar/NavBar.js
@@ -23,8 +23,6 @@ export const NavBar = () => {
 
   const toggleDropdown = () => setShowDropdown(!showDropdown);
 
-    console.log(process.env.REACT_APP_CLIENT_ID);
-
   const handleLoginClick = () => {
     setIsLoggingIn(true);
     instance.loginRedirect().catch((e) => {
